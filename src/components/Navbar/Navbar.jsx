d852effda5d614dcd353842b60f498a4271bb3be
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-05-30 20:46:44 +0530

    added navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>All</li>
+        <li>Full Stack Development</li>
+        <li>Data Science</li>
+        <li>Cyber Security</li>
+        <li>Career</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
